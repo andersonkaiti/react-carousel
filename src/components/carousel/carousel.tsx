db93de1ea8578674ce9fa1d4177ec922881a2a66
andersonkaiti <anderkaiti@gmail.com>
2024-04-28 20:50:40 -0300

    Realizando pequenas formatações no código

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./carousel.module.css";
-import { type ItemType } from "../services/shoes-services"
+import { type ItemType } from "../../services/shoes-services"
 
 type CarouselProps = {
     data: ItemType[];
@@ -8,35 +8,34 @@ type CarouselProps = {
 }
 
 export default function Carousel({ data, reference }: CarouselProps) {
-    console.log(data);
     return (
         <div
             className={styles.carousel}
             ref={reference}
         >
             {
-            data.map((item: ItemType) => {
-                return (
-                <div
-                    className={styles.item}
-                    key={item.id}
-                >
-                    <div className={styles.image}>
-                    <Image
-                        width={200}
-                        height={200}
-                        src={item.image}
-                        alt={item.name}
-                    />
-                    </div>
-                    <div className={styles.info}>
-                    <div className={styles.name}>{item.name}</div>
-                    <div className={styles.oldPrice}>US$ {item.oldPrice}</div>
-                    <div className={styles.price}>US$ {item.price}</div>
-                    </div>
-                </div>
-                );
-            })
+                data.map((item: ItemType) => {
+                    return (
+                        <div
+                            className={styles.item}
+                            key={item.id}
+                        >
+                            <div className={styles.image}>
+                                <Image
+                                    width={200}
+                                    height={200}
+                                    src={item.image}
+                                    alt={item.name}
+                                />
+                            </div>
+                                <div className={styles.info}>
+                                <div className={styles.name}>{item.name}</div>
+                                <div className={styles.oldPrice}>US$ {item.oldPrice}</div>
+                                <div className={styles.price}>US$ {item.price}</div>
+                            </div>
+                        </div>
+                    );
+                })
             }
         </div>
     )
